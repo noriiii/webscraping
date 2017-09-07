7b9612f1d8687b0c9d8c70e406059e0843252e3f
nbargo <nsbargo@gmail.com>
2017-09-06 18:30:50 -0700

    updating

--- server.js
+++ server.js
@@ -28,27 +28,43 @@ db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-// $
-request("http://www.huffingtonpost.com/", function(error, response, html) {
-  var $ = cheerio.load(html);
-
 // Main route (simple Hello World Message)
 app.get("/", function(req, res) {
-  res.send("Hello world");
+  res.send("This site works");
 });
 
-/* TODO: make two more routes
- * -/-/-/-/-/-/-/-/-/-/-/-/- */
 
-// Route 1
-// =======
-// This route will retrieve all of the data
-// from the scrapedData collection as a json (this will be populated
-// by the data you scrape using the next route)
+/* -/-/-/-/-/-/-/-/-/-/-/-/- */
+app.get("/scrape", function(req, res) {
+// Make a request call to grab the HTML body from the site of your choice
+request("http://www.huffingtonpost.com/", function(error, response, html) {
+console.log(html);
+
+// $
+  var $ = cheerio.load(html);
+
+  // Headline
+
+  $("h2.card__splash-title.bn-clickable").each(function(i, element) {
+    
+    var title = $(element).text();
+    var summary = $(element).text()
+    var link = $(element).parent().attr("href");
+
+
+    db.scrapedData.insert({ title: title, summary: summary, link: link });
+
+    res.send("Scraping is done");
+
+   });
+
+  });
+
+});
 
 app.get("/all", function(req, res) {
   // Query: In our database, go to the animals collection, then "find" everything
-  db.collections.find({}, function(error, found) {
+  db.scrapedData.find({}, function(error, found) {
     // Log any errors if the server encounters one
     if (error) {
       console.log(error);
@@ -60,83 +76,8 @@ app.get("/all", function(req, res) {
   });
 });
 
-// Route 2
-// =======
-// When you visit this route, the server will
-// scrape data from the site of your choice, and save it to
-// MongoDB.
-// TIP: Think back to how you pushed website data
-// into an empty array in the last class. How do you
-// push it into a MongoDB collection instead?
-
-/* -/-/-/-/-/-/-/-/-/-/-/-/- */
-
-function displayResults(data) {
-
-// Make a request call to grab the HTML body from the site of your choice
-request("http://www.huffingtonpost.com/", function(error, response, html) {
-console.log(html);
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-
-
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-
-  // Headline
-
-  $(".card__splash-title").each(function(i, element) {
-
-    var link = $(element).children().attr("href");
-    var title = $(element).children().text();
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
-    });
-  });
-
-  // Summary
-
-   $("card__link").each(function(i, element) {
-
-    var link = $(element).children().attr("href");
-    var summary = $(element).children().text();
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      summary: summary,
-      link: link
-    });
-  }); 
-
-
-
-
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-});
-
-}
-
-$.getJSON("/all", function(data) {
-  // Call our function to generate a table body
-  displayResults(data);
-
-
-});
-
-
-
 // Listen on port 3000
 app.listen(3000, function() {
   console.log("App running on port 3000!");
 });
 
-});
